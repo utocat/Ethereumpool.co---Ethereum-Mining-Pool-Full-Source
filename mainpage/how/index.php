86ecdd4637d3f90eebf66d992a042ccd6cca8abc
KOSZYCZEK Laurent <laurent.koszyczek@gmail.com>
2015-12-14 13:16:48 +0100

    Rename

--- mainpage/how/index.php
+++ mainpage/how/index.php
@@ -41,7 +41,7 @@
         <header id="header" class="header navbar-fixed-top">  
             <div class="container">       
                 <h1 class="logo">
-                    <a href="../"><span class="highlight">EthPool</span>utocat.com</a>
+                    <a href="../"><span class="highlight">EthPool</span>.utocat.com</a>
                 </h1><!--//logo-->
                 <nav class="main-nav navbar-right" role="navigation">
                     <div class="navbar-header">
